ab34d543d0e532bc2a98339d97a562184e18264b
deniskarandashev <39792231+deniskarandashev@users.noreply.github.com>
2024-06-15 01:21:26 +0300

    init: be

---
+++ aws-shop/src/test/java/com/karandashev/aws_shop/AwsShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karandashev.aws_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
